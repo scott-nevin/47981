b7fc944f49d1882350a5dbb96c2c85b6f4421a69
Scott Nevin <scott.nevin@att.nevin>
2013-11-21 15:18:59 -0800

    initial project 2 file
    staring point for game file. at this time, only contains comments.

---
+++ fatal_trajectory/main.cpp
@@ -0,0 +1,51 @@
+/*
+ *  Scott Nevin
+ *  November 21, 2013
+ *  Purpose: Ballistic trajectory game
+ *  Project 2
+ *
+ */
+
+//Libraries
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Global variables and constants
+const float G = 9.8;
+
+//Function prototypes
+
+
+int main(){
+
+    //Declare variables
+
+    //Display title and story
+    cout<<"\n\n Fatal Trajectory \n\n"<<endl;
+    cout<<"You are under attack! Outside your base is a tank, preparing to destroy your army."<<endl;
+    cout<<"There is a thick fog and you won't be able to destroy the tank by sight."<<endl;
+    cout<<"Your shotty sonar has determined the tank is within ___ meters."<<endl;
+    cout<<"Input the angle to aim your cannon and the speed to fire the projectile."<<endl;
+    cout<<"You'll have to destroy the tank within _ attempts or it will destroy you."<<endl;
+
+    //seed random number generator
+
+    //place tank
+
+    //Prompt user for firing instructions
+
+    //calculate projectile distance
+
+    //test if hit or miss
+
+    //if miss, repeat firing
+
+    //if hit, move tank
+
+    //
+
+
+    return 0;
+}
+
