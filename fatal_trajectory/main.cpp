7c34746ac352751b66708441b2cb0b599116216a
Scott Nevin <scott.nevin@att.nevin>
2013-11-21 19:54:57 -0800

    v3 fixed formula
    distance formula succssful

--- fatal_trajectory/main.cpp
+++ fatal_trajectory/main.cpp
@@ -16,10 +16,11 @@ using namespace std;
 
 //Global variables and constants
 const float G = 9.8;
+const float PI = 3.14;
 
 //Function prototypes
 void input(int &, int &);
-int hitMiss();
+bool hitMiss(float, int);
 int ballDst(int, int);
 int plcTnk();
 
@@ -29,10 +30,11 @@ int main(){
     //Declare variables
     int ang, power, attempt, tnkPos;
     float dist;
+    bool hit;
 
     //Display title and story
     cout<<"\n\n Fatal Trajectory \n\n"<<endl;
-    cout<<"You're under attack!\n Outside your base is a tank, preparing to destroy your army."<<endl;
+    cout<<"You're under attack!\nOutside your base is a tank, preparing to destroy your army."<<endl;
     cout<<"There is a thick fog and you won't be able to destroy the tank by sight."<<endl;
     cout<<"Your shotty sonar has determined the tank is within 1000 meters."<<endl;
     cout<<"Input the angle to aim your cannon and the speed to fire the projectile."<<endl;
@@ -47,15 +49,38 @@ int main(){
     //place tank
     tnkPos = plcTnk();
 
-    //Prompt user for firing instructions
-    input (ang, power);
+    do{
 
-    //calculate projectile distance
-    dist = ballDst(ang, power);
+        //Prompt user for firing instructions
+        input (ang, power);
 
-    cout<<"your projectile hit "<<dist<<" meters"<<endl;
+        //calculate projectile distance
+        dist = ballDst(ang, power);
 
-    //test if hit or miss
+        cout<<"your projectile hit "<<dist<<" meters"<<endl;
+
+        //test if hit or miss
+        hit = hitMiss(dist, tnkPos);
+
+        //Display if hit or miss
+        if(hit==true){
+            cout<<"Target hit!"<<endl;
+        }
+        if(hit==false){
+            cout<<"Target missed :("<<endl;
+        }
+
+        //Display if too close or too far
+        if(dist<tnkPos-10){
+            cout<<"Too close"<<endl;
+        }
+        else if(dist>tnkPos+10){
+            cout<<"Too far"<<endl;
+        }
+
+      }while(hit==false);
+
+    cout<<"tank was at "<<tnkPos<<" meters"<<endl;
 
     //if miss, repeat firing
 
@@ -82,17 +107,23 @@ void input(int &a,int &p){
         cin>>a;
         cout<<"Input projectile power in meters per second (1-100)"<<endl;
         cin>>p;
-        }while(a>90||p>500||a<0||p<0);
+        }while(a>90||p>100||a<0||p<0);
 
 }
 
 int ballDst(int a, int p){
 
-    float d, s, v;
-    s = sin(2.0*a);
-    cout<<"sin of 2 * "<<a<<" = "<<s<<endl;
-    v = p * p;
-    cout<<"velocity of "<<p<<" squared = "<<v<<endl;
-    d = (v*s)/G;
+    float d;
+    d = ((p*p)*sin(2*(a*(PI/180))))/G;
     return d;
+}
+
+bool hitMiss(float d, int t){
+
+    if (d < t-10||d > t+10){
+        return false;
+    }
+    else if (d > t-10||d < t+10){
+        return true;
+    }
 }
