d5cbe20cc3c64d470f2648f8fdb92d57839c5a03
Scott Nevin <scott.nevin@att.nevin>
2013-11-27 15:36:23 -0800

    problems 1-3

--- Assignment_6/main.cpp
+++ Assignment_6/main.cpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 using namespace std;
 
 //No GLobal Constants or variables
@@ -75,82 +76,267 @@ int getN(){
 
 void problem0(){
 
-    //Function Prototypes
-    void filArry(int [],int);
-    void prntAry(int [],int,int);
-    void swap1(int &,int &);
-    void sortPos(int [],int,int);
-    void markSort(int [],int);
+    //function prototypes
+    int High(int[], int);
+    int Low(int[], int);
 
     //Declare Variables
-       const int SIZE=9;
-        int array[SIZE];
+        const int SIZE=9;
+         int array[SIZE];
+         int high, low;
 
-       //prompt user for array
-        for (int i=1;i<11;i++){
-            cout<<"Input value "<<i<<endl;
-            cin>>array[i-1];
-        }
-
-       //Print Array
-       prntAry(array,SIZE,10);
-       cout<<endl;
-
-       //Sort the array
-        markSort(array,SIZE);
-
-        //Display results
-        cout<<"The largest number in the array is "<<array[9]<<endl;
-        cout<<"The smallest number in the array is "<<array[0]<<endl;
-
-       //Exit Stage Right
-}
-   void markSort(int a[],int n){
-         for(int i=0;i<n-1;i++){
-                sortPos(a,n,i);
-        }
-   }
-
-   void sortPos(int a[],int n,int pos){
-        if(pos>n-1)return;
-        for(int i=pos+1;i<n;i++){
-                if(a[pos]>a[i])swap1(a[pos],a[i]);
+        //prompt user for array
+         for (int i=1;i<11;i++){
+             cout<<"Input value "<<i<<endl;
+             cin>>array[i-1];
          }
-   }
 
-   void copy(int a[],int b[],int n){
-        for(int i=0;i<n;i++){
-                b[i]=a[i];
+        //determine highest and lowest
+         high = High(array, SIZE);
+         low = Low(array, SIZE);
+
+         //Display results
+         cout<<"The largest number in the array is "<<array[high]<<endl;
+         cout<<"The smallest number in the array is "<<array[low]<<endl;
+
+        //Exit Stage Right
+
+}
+
+    int High(int array[], int SIZE){
+
+        int highIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] > array[highIndx])
+                highIndx = pos;
         }
-   }
+        return highIndx;
 
-   void swap1(int &a,int &b){
-        int temp=a;
-        a=b;
-        b=temp;
-   }
+    }
+    int Low(int array[], int SIZE){
 
-   void prntAry(int a[],int n,int perLine){
-         for(int i=0;i<n;i++){
-                cout<<a[i]<<" ";
-                if(i%perLine==(perLine-1))cout<<endl;
+        int lowIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] < array[lowIndx])
+                lowIndx = pos;
         }
-        cout<<endl;
-   }
-
-   void filArry(int a[],int n){
-         for(int i=0;i<n;i++){
-                a[i]=rand()%900+100;
-        }
-   }
+        return lowIndx;
+}
 
 void problem1(){
 
+    //function prototypes
+    void getSales(string[], int[], int);
+    int getTotal(int[], int);
+    int findHigh(int[], int);
+    int findLow(int[], int);
 
+    //Declare Variables
+        const int TYPES=5;
+         int high, low, total;
+
+         //Array of salsa names
+         string names[TYPES] = {"Mild", "Hot", "Sweet", "Medium", "Zesty"};
+
+         //Array of sales
+         int sales[TYPES];
+
+         //Get number of jars sold
+         getSales(names, sales, TYPES);
+
+         //Get total sales and highest/lowest sales
+         total = getTotal(sales, TYPES);
+         high = findHigh(sales, TYPES);
+         low = findLow(sales, TYPES);
+
+         //Display sales report header
+         cout<<endl<<endl;
+         cout<<"     Salsa Sales Report \n\n";
+         cout<<"Name                Jars Sold \n";
+         cout<<"_____________________________ \n";
+
+         //Display name and jars sold
+         for (int type = 0; type < TYPES; type++){
+
+             cout<<setw(6)<<names[type]<<setw(21)<<sales[type]<<endl;
+         }
+
+         //Display total sales and high/low
+         cout<<"\nTotal Sales:"<<setw(15)<<total<<endl<<endl;
+         cout<<"High Seller: "<<names[high]<<endl;
+         cout<<"Low Seller : "<<names[low]<<endl<<endl<<endl;
+
+
+        //Exit Stage Right
+    }
+
+    void getSales(string names[], int sales[], int size){
+
+        for (int type = 0; type < size; type++){
+
+            //Get number of jars sold
+            cout<<"How many jars of "<<names[type]<<" salsa were sold last month?"<<endl;
+            cin>>sales[type];
+
+            //validate input
+            while (sales[type] < 0){
+
+                cout<<"Jars sold must be 0 or more. Please re-enter: ";
+                cin>>sales[type];
+            }
+        }
+    }
+
+    int getTotal(int array[], int size){
+
+        int total = 0;
+
+        for (int pos = 0; pos < size; pos++)
+            total += array[pos];
+
+        return total;
+    }
+
+    int findHigh(int array[], int SIZE){
+
+        int highIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] > array[highIndx])
+                highIndx = pos;
+        }
+        return highIndx;
+
+    }
+    int findLow(int array[], int SIZE){
+
+        int lowIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] < array[lowIndx])
+                lowIndx = pos;
+        }
+        return lowIndx;
 }
 
 void problem2(){
 
+    void getRain(string[], int[], int);
+    int Total(int[], int);
+    int Avg(int[], int);
+    int highRain(int[], int);
+    int lowRain(int[], int);
+
+    //Declare Variables
+        const int MONTH=12;
+         int high, low, total, avg;
+
+         //Array of salsa names
+         string names[MONTH] = {"January", "February", "March", "April", "May",
+         "June", "July", "August","September", "October", "November", "December"};
+
+         //Array of rainfall
+         int rain[MONTH];
+
+         //Get number of jars sold
+         getRain(names, rain, MONTH);
+
+         //Get total rainfall, average rainfall and highest/lowest rainfall
+         total = Total(rain, MONTH);
+         avg = Avg(rain, MONTH);
+         high = highRain(rain, MONTH);
+         low = lowRain(rain, MONTH);
+
+         //Display sales report header
+         cout<<endl<<endl;
+         cout<<"          Rainfall Report \n\n";
+         cout<<"   Month                Rainfall (inches) \n";
+         cout<<"______________________________________ \n";
+
+         //Display name and jars sold
+         for (int type = 0; type < MONTH; type++){
+
+             cout<<setw(10)<<names[type]<<setw(24)<<rain[type]<<endl;
+         }
+
+         //Display total sales and high/low
+         cout<<"\nTotal Rainfall:"<<setw(19)<<total<<endl;
+         cout<<"Average rainfall per month: "<<setw(6)<<avg<<endl;
+         cout<<"Highest rainfall month: "<<names[high]<<endl;
+         cout<<"Lowest rainfall month  : "<<names[low]<<endl;
+
+
+        //Exit Stage Right
+    }
+
+    void getRain(string names[], int rain[], int size){
+
+        for (int type = 0; type < size; type++){
+
+            //Get amount of rain
+            cout<<"How many inches of rain were there in "<<names[type]<<"?"<<endl;
+            cin>>rain[type];
+
+            //validate input
+            while (rain[type] < 0){
+
+                cout<<"Rainfall must be 0 or more. Please re-enter: ";
+                cin>>rain[type];
+            }
+        }
+    }
+
+    int Total(int array[], int size){
+
+        int total = 0;
+
+        for (int pos = 0; pos < size; pos++)
+            total += array[pos];
+
+        return total;
+    }
+
+    int Avg(int array[], int size){
+
+        int total = 0;
+        int avg;
+
+        for (int pos = 0; pos < size; pos++){
+            total += array[pos];
+        }
+        avg = total / 12;
+
+        return avg;
+    }
+
+    int highRain(int array[], int SIZE){
+
+        int highIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] > array[highIndx])
+                highIndx = pos;
+        }
+        return highIndx;
+
+    }
+    int lowRain(int array[], int SIZE){
+
+        int lowIndx = 0;
+
+        for (int pos = 1; pos < SIZE; pos++){
+
+            if (array[pos] < array[lowIndx])
+                lowIndx = pos;
+        }
+        return lowIndx;
 }
 
 void problem3(){
