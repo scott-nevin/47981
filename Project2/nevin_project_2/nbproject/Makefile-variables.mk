14012a2f8e882987384c1a39c5bab3e6032eb703
Scott Nevin <scott.nevin@att.nevin>
2014-03-11 22:11:11 -0700

    sync

--- Project2/nevin_project_2/nbproject/Makefile-variables.mk
+++ Project2/nevin_project_2/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=nevin_project_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/nevin_project_2
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/nevin_project_2
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=nevinproject2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/nevinproject2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/nevinproject2.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
 CND_ARTIFACT_NAME_Release=nevin_project_2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/nevin_project_2
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/nevin_project_2
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
 CND_PACKAGE_NAME_Release=nevinproject2.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/nevinproject2.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/nevinproject2.tar
 #
 # include compiler specific variables
 #
